bf2cb28e15305a6b804b1abad736b9e45f4a6c10
Loris Cro <kappaloris@gmail.com>
2023-12-22 00:12:34 +0100

    more scripty work

--- scripty/src/types.zig
+++ scripty/src/types.zig
@@ -4,7 +4,7 @@ const DateTime = datetime.Datetime;
 const Tokenizer = @import("Tokenizer.zig");
 
 pub const String = struct {
-    must_free: bool,
+    must_free: bool = false,
     bytes: []const u8,
 };
 
@@ -12,30 +12,13 @@ pub const Array = struct {
     must_free: bool,
     items: []Value,
 };
-// pub const ScriptFunction = *const fn ([]const Value, arena: std.mem.Allocator) ScriptResult;
-// pub const ScriptResult = union(enum) {
-//     ok: Value,
-//     err: []const u8,
-
-//     pub fn unwrap(self: ScriptResult) Value {
-//         switch (self) {
-//             .ok => |v| return v,
-//             .err => |e| @panic(e),
-//         }
-//     }
-// };
-
-// pub const ExternalValue = struct {
-//     value: *anyopaque,
-//     path: ?[]const u8 = null,
-// };
 
 pub const Result = struct {
     value: Value,
     loc: Tokenizer.Token.Loc,
 };
 
-pub const Value = union(enum) {
+pub const Value = union(Tag) {
     lazy_path: []const u8,
     date: DateTime,
     string: String,
@@ -46,6 +29,17 @@ pub const Value = union(enum) {
     err: []const u8, // error message
     nil,
 
+    pub const Tag = enum {
+        lazy_path,
+        date,
+        string,
+        bool,
+        int,
+        float,
+        array,
+        err,
+        nil,
+    };
     pub fn dot(self: Value, gpa: std.mem.Allocator, path: []const u8) error{OutOfMemory}!Value {
         _ = self;
         _ = gpa;
@@ -72,23 +66,23 @@ pub const Value = union(enum) {
             .lazy_path => unreachable,
             .float => @panic("TODO: float support in scripty"),
             .nil => @panic("TODO: explain that you dotted on a nil"),
-            inline else => |t| {
-                const Builtin = Value.builtinFor(t);
+            inline else => |v, tag| {
+                const Builtin = Value.builtinFor(tag);
                 inline for (@typeInfo(Builtin).Struct.decls) |struct_field| {
                     if (std.mem.eql(u8, struct_field.name, field)) {
-                        @field(Builtin, struct_field.name)(@field(self, t), gpa, args);
+                        return @field(Builtin, struct_field.name)(v, gpa, args);
                     }
                 }
 
-                return error.NotFound;
+                return .{ .err = "not found" };
             },
         }
     }
 
-    fn builtinFor(comptime tag: @typeInfo(Value).Union.tag_type.?) type {
+    fn builtinFor(comptime tag: Tag) type {
         return switch (tag) {
-            .lazy_path, .err, .nil => @compileError("these tags can't have builtins"),
-            .float, .nil => @panic("TODO"),
+            .lazy_path, .err => struct {},
+            .float, .nil => struct {},
             .date => DateBuiltins,
             .string => StringBuiltins,
             .bool => BoolBuiltins,
@@ -96,95 +90,131 @@ pub const Value = union(enum) {
             .array => ArrayBuiltins,
         };
     }
-
-    const DateBuiltins = struct {
-        pub fn format(dt: DateTime, gpa: std.mem.Allocator, args: []const Value) !Result {
-            const argument_error = .{ .err = "'format' wants one (string) argument" };
-            if (args.len != 1) return argument_error;
-            const string = switch (args[1]) {
-                .string => |s| s,
-                else => return argument_error,
-            };
-
-            if (!std.mem.eql(u8, string, "January 02, 2006")) {
-                @panic("TODO: implement more date formatting options");
-            }
-
-            const formatted_date = try std.fmt.allocPrint(gpa, "{s} {:0>2}, {}", .{
-                dt.date.monthName(),
-                dt.date.day,
-                dt.date.year,
-            });
-
-            return .{
-                .string = .{
-                    .must_free = true,
-                    .bytes = formatted_date,
-                },
-            };
-        }
-    };
-    const IntBuiltins = struct {
-        pub fn plus(num: usize, _: std.mem.Allocator, args: []const Value) Result {
-            const argument_error = .{ .err = "'plus' wants one (int|float) argument" };
-            if (args.len != 1) return argument_error;
-
-            switch (args[0]) {
-                .int => |add| {
-                    return .{ .int = num +| add };
-                },
-                .float => @panic("TODO: int with float argument"),
-                else => return argument_error,
-            }
-        }
-        pub fn div(num: usize, _: std.mem.Allocator, args: []const Value) Result {
-            const argument_error = .{ .err = "'div' wants one (int|float) argument" };
-            if (args.len != 1) return argument_error;
-
-            switch (args[0]) {
-                .int => |den| {
-                    const res = std.math.divTrunc(usize, num, den) catch |err| {
-                        return .{ .err = @errorName(err) };
-                    };
-
-                    return .{ .int = res };
-                },
-                .float => @panic("TODO: div with float argument"),
-                else => return argument_error,
-            }
-        }
-    };
-
-    const ArrayBuiltins = struct {
-        pub fn len(array: Array, _: std.mem.Allocator, args: []const Value) Result {
-            if (args.len != 0) return .{ .err = "'len' wants no arguments" };
-            return .{ .int = array.len };
-        }
-    };
-    const BoolBuiltins = struct {
-        pub fn not(b: bool, _: std.mem.Allocator, args: []const Value) Result {
-            if (args.len != 0) return .{ .err = "'not' wants no arguments" };
-            return .{ .bool = !b };
-        }
-    };
-    const StringBuiltins = struct {
-        pub fn len(str: String, _: std.mem.Allocator, args: []const Value) Result {
-            if (args.len != 0) return .{ .err = "'len' wants no arguments" };
-            return .{ .int = str.len };
-        }
-
-        pub fn startsWith(haystack: String, _: std.mem.Allocator, args: []const Value) Result {
-            const argument_error = .{
-                .err = "'startsWith' wants one (string) argument, the needle",
-            };
-
-            if (args.len != 1) return argument_error;
-            const needle = switch (args[1]) {
-                .string => |s| s,
-                else => return argument_error,
-            };
-
-            return .{ .bool = std.mem.startsWith(u8, haystack, needle) };
-        }
-    };
 };
+
+pub const DateBuiltins = struct {
+    pub fn format(dt: DateTime, gpa: std.mem.Allocator, args: []const Value) !Value {
+        const bad_arg = .{ .err = "'format' wants one (string) argument" };
+        if (args.len != 1) return bad_arg;
+        const string = switch (args[1]) {
+            .string => |s| s,
+            else => return bad_arg,
+        };
+
+        if (!std.mem.eql(u8, string.bytes, "January 02, 2006")) {
+            @panic("TODO: implement more date formatting options");
+        }
+
+        const formatted_date = try std.fmt.allocPrint(gpa, "{s} {:0>2}, {}", .{
+            dt.date.monthName(),
+            dt.date.day,
+            dt.date.year,
+        });
+
+        return .{
+            .string = .{
+                .must_free = true,
+                .bytes = formatted_date,
+            },
+        };
+    }
+};
+pub const IntBuiltins = struct {
+    pub fn plus(num: usize, _: std.mem.Allocator, args: []const Value) Value {
+        const bad_arg = .{ .err = "'plus' wants one (int|float) argument" };
+        if (args.len != 1) return bad_arg;
+
+        switch (args[0]) {
+            .int => |add| {
+                return .{ .int = num +| add };
+            },
+            .float => @panic("TODO: int with float argument"),
+            else => return bad_arg,
+        }
+    }
+    pub fn div(num: usize, _: std.mem.Allocator, args: []const Value) Value {
+        const bad_arg = .{ .err = "'div' wants one (int|float) argument" };
+        if (args.len != 1) return bad_arg;
+
+        switch (args[0]) {
+            .int => |den| {
+                const res = std.math.divTrunc(usize, num, den) catch |err| {
+                    return .{ .err = @errorName(err) };
+                };
+
+                return .{ .int = res };
+            },
+            .float => @panic("TODO: div with float argument"),
+            else => return bad_arg,
+        }
+    }
+};
+
+pub const ArrayBuiltins = struct {
+    pub fn len(array: Array, _: std.mem.Allocator, args: []const Value) Value {
+        if (args.len != 0) return .{ .err = "'len' wants no arguments" };
+        return .{ .int = array.items.len };
+    }
+};
+pub const BoolBuiltins = struct {
+    pub fn not(b: bool, _: std.mem.Allocator, args: []const Value) Value {
+        if (args.len != 0) return .{ .err = "'not' wants no arguments" };
+        return .{ .bool = !b };
+    }
+};
+pub const StringBuiltins = struct {
+    pub fn len(str: String, _: std.mem.Allocator, args: []const Value) Value {
+        if (args.len != 0) return .{ .err = "'len' wants no arguments" };
+        return .{ .int = str.bytes.len };
+    }
+
+    pub fn startsWith(haystack: String, _: std.mem.Allocator, args: []const Value) Value {
+        const bad_arg = .{
+            .err = "'startsWith' wants one (string) argument, the needle",
+        };
+
+        if (args.len != 1) return bad_arg;
+        const needle = switch (args[1]) {
+            .string => |s| s,
+            else => return bad_arg,
+        };
+
+        return .{ .bool = std.mem.startsWith(u8, haystack.bytes, needle.bytes) };
+    }
+};
+
+// pub fn PathNavigator(comptime T: type) type {
+//     return struct {
+//         const info = @typeInfo(T).Struct;
+//         pub fn dot(
+//             op: *anyopaque,
+//             gpa: std.mem.Allocator,
+//             path: []const u8,
+//         ) error{OutOfMemory}!Value {
+//             const t: *T = @alignCast(@ptrCast(op));
+//             var it = std.mem.tokenizeScalar(u8, path, '.');
+//             const component = it.next().?;
+//             const rest = it.rest();
+
+//             for (info.fields) |f| {
+//                 if (std.mem.eql(u8, f.name, component)) {
+//                     if (Value.supports(f.type))
+//                     const FieldNav = PathNavigator(f.type);
+//                     const field_ptr = &@field(t, f.name);
+//                     if (rest.len == 0) {
+//                         return .{
+//                             .global = .{
+//                                 .ptr = field_ptr,
+//                                 .fn_ptr = &FieldNav.dot,
+//                             },
+//                         };
+//                     } else {
+//                         return FieldNav.dot(field_ptr, gpa, rest);
+//                     }
+//                 }
+//             }
+
+//             return .{ .err = "not found" };
+//         }
+//     };
+// }
