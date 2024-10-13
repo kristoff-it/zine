3ccd7220b5917400455ac7a8f197fc951b468739
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-10-14 01:44:47 +0200

    Fix structs for template

--- src/context/Git.zig
+++ src/context/Git.zig
@@ -3,6 +3,7 @@ const Git = @This();
 const std = @import("std");
 const scripty = @import("scripty");
 const context = @import("../context.zig");
+const Signature = @import("doctypes.zig").Signature;
 const DateTime = context.DateTime;
 const String = context.String;
 const Optional = context.Optional;
@@ -19,19 +20,19 @@ commit_message: []const u8,
 author_name: []const u8,
 author_email: []const u8,
 
-tag: ?[]const u8,
-branch: ?[]const u8,
+@"tag?": ?[]const u8,
+@"branch?": ?[]const u8,
 
 pub fn init() Git {
     return .{
-        ._in_repo = false,
-        .commit_hash = "",
+        ._in_repo = true,
+        .commit_hash = "TestHash",
         .commit_date = DateTime.initNow(),
-        .commit_message = "",
-        .author_name = "",
-        .author_email = "",
-        .tag = null,
-        .branch = null,
+        .commit_message = "TestCommit",
+        .author_name = "Marlon",
+        .author_email = "@mail",
+        .@"tag?" = "tag",
+        .@"branch?" = "branch",
     };
 }
 
@@ -40,5 +41,29 @@ fn readHead() ![]const u8 {}
 pub const description =
     \\Information about the current git repository.
 ;
+
+pub const Fields = struct {
+    pub const commit_hash =
+        \\The current commit hash.
+    ;
+    pub const commit_date =
+        \\The date of the current commit.
+    ;
+    pub const commit_message =
+        \\The commit message of the current commit.
+    ;
+    pub const author_name =
+        \\The name of the author of the current commit.
+    ;
+    pub const author_email =
+        \\The email of the author of the current commit.
+    ;
+    pub const @"tag?" =
+        \\The current tag, if any.
+    ;
+    pub const @"branch?" =
+        \\The current branch, if any.
+    ;
+};
 
 pub const Builtins = struct {};
