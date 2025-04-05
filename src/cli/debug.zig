346911471633aad53d81e5e036d0bc07bdcb8753
Loris Cro <kappaloris@gmail.com>
2025-04-05 18:22:36 +0200

    update zine debug

--- src/cli/debug.zig
+++ src/cli/debug.zig
@@ -13,10 +13,11 @@ pub fn debug(
     gpa: Allocator,
     args: []const []const u8,
 ) bool {
-    _ = args;
     errdefer |err| switch (err) {
         error.OutOfMemory => fatal.oom(),
     };
+
+    const cmd: Command = try .parse(gpa, args);
 
     const cfg, const base_dir_path = root.Config.load(gpa);
 
@@ -91,20 +92,23 @@ pub fn debug(
             for (s.pages.items) |p_idx| {
                 const p = variant.pages.items[p_idx];
 
-                std.debug.print("    {} ({})", .{
+                std.debug.print("    {}", .{
                     p._scan.file.fmt(
                         &variant.string_table,
                         &variant.path_table,
                         variant.content_dir_path,
                     ),
-                    p_idx,
                 });
 
-                if (p._scan.subsection_id != 0) {
-                    std.debug.print(" [{}]\n", .{p._scan.subsection_id});
-                } else {
-                    std.debug.print("\n", .{});
+                if (cmd.ids) {
+                    std.debug.print(" ({})", .{p_idx});
+
+                    if (p._scan.subsection_id != 0) {
+                        std.debug.print(" [{}]", .{p._scan.subsection_id});
+                    }
                 }
+
+                std.debug.print("\n", .{});
             }
 
             std.debug.print("],\n\n", .{});
@@ -135,3 +139,91 @@ pub fn debug(
 
     return false;
 }
+
+pub const Command = struct {
+    build_assets: std.StringArrayHashMapUnmanaged(BuildAsset),
+    drafts: bool,
+    ids: bool,
+
+    pub fn deinit(co: *const Command, gpa: Allocator) void {
+        var ba = co.build_assets;
+        ba.deinit(gpa);
+    }
+
+    pub fn parse(gpa: Allocator, args: []const []const u8) !Command {
+        var build_assets: std.StringArrayHashMapUnmanaged(BuildAsset) = .empty;
+        var drafts = false;
+        var ids = false;
+
+        const eql = std.mem.eql;
+        const startsWith = std.mem.startsWith;
+        var idx: usize = 0;
+        while (idx < args.len) : (idx += 1) {
+            const arg = args[idx];
+            if (eql(u8, arg, "-h") or eql(u8, arg, "--help")) {
+                fatal.msg(help_message, .{});
+            } else if (startsWith(u8, arg, "--build-asset=")) {
+                const name = arg["--build-asset=".len..];
+
+                idx += 1;
+                if (idx >= args.len) fatal.msg(
+                    "error: missing build asset sub-argument for '{s}'",
+                    .{name},
+                );
+
+                const input_path = args[idx];
+
+                idx += 1;
+                var output_path: ?[]const u8 = null;
+                var output_always = false;
+                if (idx < args.len) {
+                    const next = args[idx];
+                    if (startsWith(u8, next, "--output=")) {
+                        output_path = next["--output=".len..];
+                    } else if (startsWith(u8, next, "--output-always=")) {
+                        output_always = true;
+                        output_path = next["--output-always=".len..];
+                    } else {
+                        idx -= 1;
+                    }
+                }
+
+                const gop = try build_assets.getOrPut(gpa, name);
+                if (gop.found_existing) fatal.msg(
+                    "error: duplicate build asset name '{s}'",
+                    .{name},
+                );
+
+                gop.value_ptr.* = .{
+                    .input_path = input_path,
+                    .output_path = output_path,
+                    .output_always = output_always,
+                    .rc = .{ .raw = @intFromBool(output_always) },
+                };
+            } else if (eql(u8, arg, "--drafts")) {
+                drafts = true;
+            } else if (eql(u8, arg, "--ids")) {
+                ids = true;
+            } else {
+                fatal.msg("error: unexpected cli argument '{s}'\n", .{arg});
+            }
+        }
+
+        return .{
+            .build_assets = build_assets,
+            .drafts = drafts,
+            .ids = ids,
+        };
+    }
+};
+
+const help_message =
+    \\Usage: zine debug [OPTIONS]
+    \\
+    \\Command specific options:
+    \\  --ids        Include ids when printing info. Snapshot unsafe!
+    // \\  --build-assets FILE    Path to a file containing a list of build assets
+    \\  --help, -h   Show this help menu
+    \\
+    \\
+;
