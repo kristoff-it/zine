6419141cca1f3ad8f266fb59501cf6da8dbbc041
Niklas Koll <nkoll@databay.de>
2025-07-08 10:48:15 +0200

    Modify flake, update version number in build.zig.zon

--- flake.nix
+++ flake.nix
@@ -6,37 +6,42 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      zig2nix,
-      ...
-    }:
-    let
-      inherit (nixpkgs) lib;
-      forAllSystems =
-        body: lib.genAttrs lib.systems.flakeExposed (system: body nixpkgs.legacyPackages.${system});
-    in
-    {
-      packages = forAllSystems (
-        pkgs:
-        let
-          env = zig2nix.outputs.zig-env.${pkgs.system} {
+  outputs = {
+    self,
+    nixpkgs,
+    zig2nix,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    forAllSystems = body:
+      lib.genAttrs lib.systems.flakeExposed (system:
+        body {
+          inherit system;
+          pkgs = nixpkgs.legacyPackages.${system};
+          env = zig2nix.outputs.zig-env.${system} {
             nixpkgs = nixpkgs;
-            zig = pkgs.zig;
           };
-        in
-        {
-          zine = env.package {
-            src = lib.cleanSource ./.;
-            nativeBuildInputs = [ ];
-            buildInputs = [ ];
-            zigPreferMusl = false;
-          };
-          default = self.packages.${pkgs.system}.zine;
-        }
-      );
-      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
-    };
+        });
+  in {
+    packages = forAllSystems (
+      {
+        system,
+        env,
+        ...
+      }: {
+        zine = env.package {
+          src = lib.cleanSource ./.;
+          nativeBuildInputs = [];
+          buildInputs = [];
+          zigPreferMusl = false;
+        };
+        default = self.packages.${system}.zine;
+      }
+    );
+    devShells = forAllSystems (
+      {env, ...}: {
+        default = env.mkShell {};
+      }
+    );
+  };
 }
