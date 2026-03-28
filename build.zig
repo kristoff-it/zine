62a2c1500f6cc47b0e73279aad1127e386186855
David Marker <dave@freedave.net>
2026-03-28 12:55:22 -0300

    Add code to build on FreeBSD. This is similar to what other BSDs could use as well (with libinotify) but does not fix #112. The update to afl_kit seemed to be required when FreeBSD updated its version of zig to 0.15.2.

--- build.zig
+++ build.zig
@@ -309,6 +309,11 @@ pub fn build(b: *std.Build) !void {
         else => @panic("target must be added to build.zig"),
         .linux => {},
 
+       .freebsd => {
+            // only required for FreeBSD < 15
+            // zine_exe.linkSystemLibrary("inotify");
+        },
+
         .windows => {
             zine_exe.linkSystemLibrary("ws2_32");
         },
@@ -519,8 +524,10 @@ fn setupReleaseStep(
     const targets: []const std.Target.Query = &.{
         .{ .cpu_arch = .aarch64, .os_tag = .macos },
         .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .musl },
+        .{ .cpu_arch = .aarch64, .os_tag = .freebsd },
         .{ .cpu_arch = .x86_64, .os_tag = .macos },
         .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+        .{ .cpu_arch = .x86_64, .os_tag = .freebsd },
         .{ .cpu_arch = .x86_64, .os_tag = .windows },
         .{ .cpu_arch = .aarch64, .os_tag = .windows },
     };
@@ -626,6 +633,10 @@ fn setupReleaseStep(
         switch (target.result.os.tag) {
             else => @panic("target must be added to build.zig"),
             .linux => {},
+            .freebsd => {
+                // only required for FreeBSD < 15
+                // zine_exe_release.linkSystemLibrary("inotify");
+            },
             .windows => {
                 zine_exe_release.linkSystemLibrary("ws2_32");
             },
