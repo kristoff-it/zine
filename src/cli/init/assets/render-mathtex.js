a6ff65f6425e9f872a1f7707c6779df2a20f22e4
Jack DeVries <jdevries3133@gmail.com>
2026-05-09 19:47:50 -0400

    chore: remove trailing whitespace from init template

--- src/cli/init/assets/render-mathtex.js
+++ src/cli/init/assets/render-mathtex.js
@@ -2,7 +2,7 @@ let eqns = document.querySelectorAll("script[type='math/tex']");
 for (let i=eqns.length-1; i>=0; i--) {
     let eqn = eqns[i];
     let src = eqn.text;
-    let d = eqn.closest('p') == null; 
+    let d = eqn.closest('p') == null;
     eqn.outerHTML = temml.renderToString(src, { displayMode: d });
 }
 
